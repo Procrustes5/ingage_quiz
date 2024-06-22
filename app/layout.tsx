ab2ab0f4a0452eefaaf42a4711f90ce5848f99e2
Hanse Kim <procrustes5@gmail.com>
2024-06-22 18:01:13 +0900

    Next UI導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Providers } from "./provider";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -19,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+        <Providers>
           {children}
-        </main>
+        </Providers>
       </body>
     </html>
   );
