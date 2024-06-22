4f171a305ae46d242095ddda4195e9571f0553a0
Hanse Kim <procrustes5@gmail.com>
2024-06-22 19:31:51 +0900

    biome

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,23 @@
-import {nextui} from '@nextui-org/theme';
+import { nextui } from '@nextui-org/theme'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
+          background: 'hsl(var(--btn-background))',
+          'background-hover': 'hsl(var(--btn-background-hover))',
         },
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
-};
+}
