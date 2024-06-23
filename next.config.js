d829fd39ac0e7f2b3cee74895af901e6b5bad52c
jinyang <youkeitou327@gmail.com>
2024-06-23 21:18:41 +0900

    next.js webpackの設定を変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { _isServer }) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
 
 module.exports = nextConfig;
